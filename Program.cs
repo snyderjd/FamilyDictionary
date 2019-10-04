4f4b56fde16f4f126a3ccd7b69ea2f422f43f0e9
snyder <snyderjosephd@yahoo.com>
2019-10-04 15:33:56 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FamilyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+NSS C# Dictionaries Practice: FamilyDictionary challenge exercise
